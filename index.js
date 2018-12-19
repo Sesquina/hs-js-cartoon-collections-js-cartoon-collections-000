3da5060f6883136708ec8e4e66848f47e49ef815
essential-transformation-0497 <samiraesquina@gmail.com>
2018-12-19 07:15:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
+//Dwarf Roll Call
+var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"];
 function dwarfRollCall(dwarves) {
-}
+var array =[];
+   for (var i=0;i<dwarves.length;i++){
+     array.push(`${i+1}. ${dwarves[i]} `);
+   }
+   return array.join("");
+ }
 
+
+
+//Summon Captain Planet
+var planeteerCalls = ["earth", "wind", "fire", "water", "heart"];
 function summonCaptainPlanet(planeteerCalls){
-}
+ return planeteerCalls.map(a=>a.toUpperCase()+"!");
+ }
 
-function longPlaneteerCalls(words) {
-}
 
+// Long Planeteer Calls
+function longPlaneteerCalls(words){
+  for(var i=0;i<words.length;i++){
+    if (words[i].length>4){
+      return true;
+    }
+  }
+  return false;
+}
+ 
+var words = ["wind", "fire"];
+longPlaneteerCalls(words); 
+
+
+
+//Find The Cheese
 function findTheCheese (foods) {
+  for(var i=0;i<foods.length;i++){
+  if (foods[i]=="cheddar"
+  ||foods[i]=="gouda"
+  ||foods[i]=="camembert"
+  ){
+    return foods[i];
+  }
+}
+return "no cheese!";
 }
